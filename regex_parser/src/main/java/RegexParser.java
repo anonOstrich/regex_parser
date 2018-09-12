b44c77ddcf6a0f312ca5e230067f594820b8a92a
Jesper Kuutti <jesper.kuutti@helsinki.fi>
2018-09-12 09:18:18 +0300

    Started to create an NFA creator
    Mainly committing in order to change from one computer to another
    b

--- regex_parser/src/main/java/RegexParser.java
+++ regex_parser/src/main/java/RegexParser.java
@@ -1,12 +1,14 @@
 
 import domain.NFA;
+import utils.AutomatonTools; 
 
 public class RegexParser {
 
     public static void main(String[] args) {
+        AutomatonTools creator = new AutomatonTools(); 
         String pattern = "Y";
         String test = "Y";
-        NFA nfa = new NFA(pattern);
+        NFA nfa = creator.transformIntoNFA(pattern);
         System.out.println("Automaatin rakenne:\n" + nfa);
         
         System.out.println("NFA accepts the string " + test  +":\n" + nfa.accepts(test));
--- regex_parser/src/main/java/utils/AutomatonTools.java
+++ regex_parser/src/main/java/utils/AutomatonTools.java
@@ -1,29 +1,51 @@
 package utils;
 
-import domain.NFA; 
+import java.util.Set;
+import java.util.HashSet;
+
+import domain.NFA;
+import domain.State;
 
 public class AutomatonTools {
-    
-    public static NFA createNegationFor(String pattern){
-        
+
+    private Set<Character> alphabet;
+    private int lowestAvailable;
+
+    public AutomatonTools() {
+        alphabet = new HashSet();
+        lowestAvailable = 0;
+
+        for (int i = (int) 'A'; i <= (int) 'z'; i++) {
+            alphabet.add((char) i);
+        }
+        for (int i = (int) '0'; i <= (int) '9'; i++) {
+            alphabet.add((char) i);
+        }
+    }
+
+    public NFA transformIntoDFA(String pattern) {
+
+        NFA dfa = new NFA();
+
+        if (pattern.length() == 1 && (pattern.charAt(0) == '#' || alphabet().contains(pattern.charAt(0)))) {
+            State s1 = new State(lowestAvailable);
+            State s2 = new State(lowestAvailable + 1);
+            lowestAvailable += 2;
+            nfa.addTransition(s1, pattern.charAt(0), s2);
+
+        }
+
+        return dfa;
+    }
+
+    public static NFA createNegationFor(String pattern) {
+
         // first create a corresponding NFA
         NFA nfa = new NFA(pattern);
-        
 
         // then transorm it into a DFA
-        
-        
         // then swap accepting and non-accepting states
-        
-        return nfa; 
+        return nfa;
     }
-    
-    private static NFA transformIntoDFA(NFA nfa){
-        NFA dfa = new NFA(); 
-        
-        return dfa; 
-    }
-    
+
 }
-
-
